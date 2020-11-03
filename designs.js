f450a358df8088e3267058442b4ff4d918b42fd1
MatskiB <32953047+MatskiB@users.noreply.github.com>
2020-11-03 20:43:24 +0100

    Add files via upload

--- designs.js
+++ designs.js
@@ -1,15 +1,24 @@
+// variable for the element that determines the table's heigth
 const gridHeight = document.getElementById("inputHeight");
+// variable for the element that determines the table's width
 const gridWidth = document.getElementById("inputWidth");
+// variable for the form presented to the user that enables them to create a canvas
 const gridSize = document.getElementById("sizePicker");
+// variable for the canvas
 const grid = document.getElementById("pixelCanvas");
+// variable for the color picker tool
 const color = document.getElementById("colorPicker");
 
+
+// Event listener for the grid size that calls makeGrid function after
+// receiving input from the user
 gridSize.addEventListener("submit", function(event) {
   event.preventDefault();
   makeGrid(gridHeight.value, gridWidth.value)
 });
 
-
+// makeGrid function that creates a canvas for the user and listens to
+// color input
 function makeGrid(height, width) {
   grid.innerHTML = " ";
   for (let i = 0; i < height; i++) {
